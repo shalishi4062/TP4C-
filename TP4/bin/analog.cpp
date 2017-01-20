7f2e32a095b5cce2669092eff630bf9e6c1c81f9
shikshak <ft.mezidi@gmail.com>
2017-01-20 20:51:35 +0100

    Add files via upload

--- TP4/bin/analog.cpp
+++ TP4/bin/analog.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <string>
 #include "CollectionDeLog.h"
 #include <iostream>
@@ -10,26 +11,31 @@ int main(int argc,char*argv[])
 	int gflag=0;//3 drapeaux par defaut 0. Si il y a des options,on mettra ces valeurs a 1
 	int heure;
 	string nomdegraphe;
-	string nomdeficher;
+	string nomdefichier;
 
 	if(argc==2){
-		nomdeficher=argv[1];	
-		if(nomdeficher.find(".log")==std::string::npos){
+		nomdefichier=argv[1];	
+		if(nomdefichier.find(".log")==std::string::npos){
 			cout<<"The name of source document must be end with \".log\"."<<endl;
 			return 0;
 		}
-		CollectionDeLog collection(nomdeficher, 'n');
-	}//Si il n 'y a pas de options,appeler le constructeur sans options
+		CollectionDeLog collection(nomdefichier, 'n');
+	}//S'il n'y a pas de options,appeler le constructeur sans options
 
+	if(argc <=1){
+		cout << "input some argument" << endl;
+		return 0;	
+	}//Si aucun argument n'est entre
 
 	if(argc>2)
 	{
-		string nomdeficher=argv[argc-1];
-		if(nomdeficher.find(".log")==std::string::npos)
+		nomdefichier=argv[argc-1];
+
+		if(nomdefichier.find(".log")==std::string::npos)
 		{
 			cout<<"The name of source document must be end with \".log\"."<<endl;
 			return 0;
-		}//Si nomdeficher est pas correct,output erreur.
+		}//Si nomdefichier est pas correct,output erreur.
 
 		char option=' ';
 		for(int i=1;i<(argc-1);i++)
@@ -48,7 +54,7 @@ int main(int argc,char*argv[])
 					int time1=heure1-'0';
 					int time2=heure2-'0';
 					heure=time1*10+time2;//calculer l'heure
-					if(heure>24||heure<0){cout<<"Invalid time!"<<endl;return 0;}
+					if(heure>=24||heure<0){cout<<"Invalid time!"<<endl;return 0;}
 				}
 				catch(exception e){  //quand l'heure a seulement 1 chiffre,on fait ceux ci
 					string heures=argv[i+1];
@@ -68,16 +74,16 @@ int main(int argc,char*argv[])
 			case('g'):{		
 				nomdegraphe=argv[i+1];
 				if(nomdegraphe.find(".dot")==std::string::npos){
-					cout<<"Name of the photo must be end with\".dot\"."<<endl;
+					cout<<"Name of the GraphViz file must be end with\".dot\"."<<endl;
 					return 0;
-				}//grader le nom de graphe ,output erreur quand il na pas de forme correcte
+				}//garder le nom de graphe ,output erreur quand il na pas de forme correcte
 				gflag=1;//mettre gflag a 1;
 				i++;
 				break;
 					  }
 			default:{
 				cout<<"Wrong command"<<endl;
-				return 0;//a part de tous les sympols corrects,les autre tous sont fautes
+				return 0;//a part les symboles corrects,les autres sont tous fauts
 					}
 
 
@@ -87,43 +93,43 @@ int main(int argc,char*argv[])
 
 	if(gflag==1&&eflag==0&&tflag==0)
 	{
-		CollectionDeLog collection(nomdeficher,'n',-1, 'g', nomdegraphe);
+		CollectionDeLog collection(nomdefichier,'n',-1, 'g', nomdegraphe);
 		return 0;
 	}
 	if(gflag==1&&eflag==1&&tflag==1)
 	{
-		CollectionDeLog collection(nomdeficher, 'e', heure,'g', nomdegraphe);
+		CollectionDeLog collection(nomdefichier, 'e', heure,'g', nomdegraphe);
 		return 0;
 	}
 
 	if(gflag==1&&eflag==1&&tflag==0)
 	{
-		CollectionDeLog collection(nomdeficher,'e',-1, 'g', nomdegraphe);
+		CollectionDeLog collection(nomdefichier,'e',-1, 'g', nomdegraphe);
 		return 0;
 	}
 
 	if(gflag==1&&eflag==0&&tflag==1)
 	{
-		CollectionDeLog collection(nomdeficher, 'n',heure, 'g', nomdegraphe);
+		CollectionDeLog collection(nomdefichier, 'n',heure, 'g', nomdegraphe);
 		return 0;
 	}
 
 	if(gflag==0&&eflag==1&&tflag==1)
 	{
-		CollectionDeLog collection(nomdeficher, 'e',heure);
+		CollectionDeLog collection(nomdefichier, 'e',heure);
 		return 0;
 	}
 
 	if(gflag==0&&eflag==1&&tflag==0)
 	{
-		CollectionDeLog collection(nomdeficher, 'e');
+		CollectionDeLog collection(nomdefichier, 'e');
 		return 0;
 	}
 
 	if(gflag==0&&eflag==0&&tflag==1)
 	{
 
-		CollectionDeLog collection(nomdeficher,'n',heure);
+		CollectionDeLog collection(nomdefichier,'n',heure);
 		return 0;
 	}
 	return 0;
