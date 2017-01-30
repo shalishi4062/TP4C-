e84ae31177e8ad99afd49f8fab46a116ae5b0267
shalishi4062 <shali.shi@insa-lyon.fr>
2017-01-30 15:29:07 +0100

    Update analog.cpp

--- TP4/bin/analog.cpp
+++ TP4/bin/analog.cpp
@@ -15,14 +15,14 @@ int main(int argc,char*argv[]) {
 		nomdefichier = argv[1];
 		if (nomdefichier.find(".log") == nomdefichier.npos) {
 			cerr << "The name of source document must be end with \".log\"." << endl;
-			return 0;
+			return 2;
 		}
 		FILE *file = NULL;
 		file = fopen(nomdefichier.c_str(), "r");
 
 		if (file == NULL) {
 			cerr << "The file does not exist or you do not have the reading rights on it." << endl;;
-			return 0;
+			return 3;
 		} // si le fichier n'existe pas
 		fclose(file);
 
@@ -31,7 +31,7 @@ int main(int argc,char*argv[]) {
 
 	if (argc <= 1) {
 		cerr << "input some argument" << endl;
-		return 0;
+		return 1;
 	}//Si aucun argument n'est entre
 
 	if (argc > 2) {
@@ -39,14 +39,14 @@ int main(int argc,char*argv[]) {
 
 		if (nomdefichier.find(".log") == nomdefichier.npos) {
 			cerr << "The name of source document must be end with \".log\"." << endl;
-			return 0;
+			return 2;
 		}//Si nomdefichier est pas correct,output erreur.
 		FILE *file = NULL;
 		file = fopen(nomdefichier.c_str(), "r");
 
 		if (file == NULL) {
 			cerr << "The file does not exist or you do not have the reading rights on it." << endl;
-			return 0;
+			return 3;
 		} // si le fichier n'existe pas
 		fclose(file);
 
@@ -54,41 +54,54 @@ int main(int argc,char*argv[]) {
 		for (int i = 1; i < (argc - 1); i++) {
 			if (argv[i][0] != '-') {
 				cerr << "argument should start with '-'" << endl;
-				return 0;
+				return 4;
 			} //s'il manque le -
 			if (argv[i][2] != '\0') {
 				cerr << "argument should be only one character" << endl;
-				return 0;
+				return 4;
 			} // si l'argument a plus d'une lettre
 			option = argv[i][1]; // option prend la valeur de l'option
 			switch (option) {
 			case ('t'): {
-
+                                if(tflag==1) 
+				{
+					cout << "You already wrote the -t argument" << endl;
+					return 6;
+				}
 				string heures = argv[i + 1];
 				if(!isdigit(heures[0])){
 					cerr << "You must precise the hour with command -t." <<endl;
-					return 0;
+					return 5;
 				}
 				heure = stoi(heures);
 				if (heure >= 24 || heure < 0) {
 					cerr << "Invalid time ! " << endl;
-					return 0;
+					return 5;
 				}
 				i++;//i plus 1 ,pour obtenir la prochaine option
-				if(tflag==1) cout << "Warning : you already wrote the -t argument" << endl;
+				
 				tflag = 1;//mettre tflag a 1;
 				break;
 			}
 			case ('e'): {
-				if(eflag==1) cout << "Warning : you already wrote the -e argument" << endl;
+				  if(eflag==1) 
+				{
+					cout << "You already wrote the -e argument" << endl;
+					return 6;
+				}
 				eflag = 1;//mettre eflag a 1;
 				break;
 			}
 			case ('g'): {
+				  if(gflag==1) 
+				{
+					cout << "You already wrote the -g argument" << endl;
+					return 6;
+				}
 				nomdegraphe = argv[i + 1];
 				if (nomdegraphe.find(".dot") == nomdegraphe.npos) {
 					cerr << "Name of the GraphViz file must be end with\".dot\"." << endl;
-					return 0;
+					return 5;
 				}//erreur quand il n a pas de forme correcte
 
 				//Test existante fichier graph
@@ -110,7 +123,7 @@ int main(int argc,char*argv[]) {
 							cin >> nomdegraphe;
 							if (nomdegraphe.find(".dot") == nomdegraphe.npos) {
 								cerr << "Name of the GraphViz file must be end with\".dot\"." << endl;
-								return 0;
+								return 5;
 							}//erreur quand il n a pas de forme correcte
 
 						}else{
@@ -126,6 +139,7 @@ int main(int argc,char*argv[]) {
 			}
 			default: {
 				cout << "Wrong command" << endl; //a part les symboles corrects,les autres sont tous fauts
+			return 7;				
 			}
 
 
