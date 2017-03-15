26b0502061b61eee92be0fbc5d1ee0cf2eae74b3
shikshak <ft.mezidi@gmail.com>
2017-03-15 17:00:58 +0100

    Add files via upload

--- GustatIF_B3431/src/main/java/metier/service/ServiceMetier.java
+++ GustatIF_B3431/src/main/java/metier/service/ServiceMetier.java
@@ -5,10 +5,99 @@
  */
 package metier.service;
 
+import com.google.maps.model.LatLng;
+import dao.ClientDAO;
+import dao.JpaUtil;
+import dao.RestaurantDAO;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import metier.modele.Client;
+import metier.modele.Commande;
+import metier.modele.Produit;
+import metier.modele.Restaurant;
+import static util.GeoTest.getLatLng;
+
 /**
  *
  * @author jcharlesni
  */
 public class ServiceMetier {
     
+    public static void signUpClient(String nom, String prenom, String mail, String adresse) {
+        
+        LatLng latlong = getLatLng(adresse);
+        
+        JpaUtil.init();
+        JpaUtil.creerEntityManager();
+        ClientDAO cdao = new ClientDAO();
+        Client cl = new Client(nom, prenom, mail, adresse);
+        try {
+            JpaUtil.ouvrirTransaction();
+            cdao.create(cl);
+            JpaUtil.validerTransaction();
+            ServiceTechnique.envoiMailInscription(1,cl);
+        } catch (Exception ex) {
+            Logger.getLogger(ServiceMetier.class.getName()).log(Level.SEVERE, null, ex);
+            ServiceTechnique.envoiMailInscription(0,cl);
+        }
+        JpaUtil.fermerEntityManager();
+        JpaUtil.destroy();
+    }
+    
+    public static Client singInClient(String mail){
+        JpaUtil.init();
+        JpaUtil.creerEntityManager();
+        ClientDAO cdao = new ClientDAO();
+        Client cl = null;
+        try {
+            List<Client> clients = cdao.findAll(); 
+            for (int i = 0; i < clients.size(); i++) {
+		if(clients.get(i).getMail().equals(mail)){
+                    System.out.println("Bienvenue "+cl.getPrenom());
+                    JpaUtil.fermerEntityManager();
+                    JpaUtil.destroy();
+                    return clients.get(i);
+                }
+	}
+        } catch (Exception ex) {
+            Logger.getLogger(ServiceMetier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Vous n'êtes pas encore inscrit!");
+        JpaUtil.fermerEntityManager();
+        JpaUtil.destroy();
+        return cl;
+    }
+    
+    public static List<Restaurant> consultListRestaurant (){
+        JpaUtil.init();
+        JpaUtil.creerEntityManager();
+        List<Restaurant> restos = null;
+        RestaurantDAO rdao= new RestaurantDAO();
+        try {     
+            restos = rdao.findAll();          
+        } catch (Exception ex) {
+            Logger.getLogger(ServiceMetier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        JpaUtil.fermerEntityManager();
+        JpaUtil.destroy();
+        return restos;
+    }
+    
+      public static List<Produit> consultListProduit (long idr){
+            JpaUtil.init();
+            JpaUtil.creerEntityManager();
+            Restaurant resto = null;
+            RestaurantDAO rdao= new RestaurantDAO();
+        try {     
+            resto = rdao.findById(idr);
+        } catch (Exception ex) {
+            Logger.getLogger(ServiceMetier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        JpaUtil.fermerEntityManager();
+        JpaUtil.destroy();
+        return resto.getProduits();
+    }    
+      
+      
 }
--- GustatIF_B3431/src/main/java/metier/service/ServiceTechnique.java
+++ GustatIF_B3431/src/main/java/metier/service/ServiceTechnique.java
@@ -5,10 +5,41 @@
  */
 package metier.service;
 
+import metier.modele.Client;
+import metier.modele.Commande;
+import metier.modele.Livreur;
+import metier.modele.LivreurHumain;
+
 /**
  *
  * @author jcharlesni
  */
 public class ServiceTechnique {
     
+    public static void envoiMailInscription(int etat, Client cl){
+        //si 0 echec si 1 reussi
+        System.out.println("Expediteur : gustatif@gustatif.com\n"
+                    + "Pour : " + cl.getMail() +"\n"
+                            + "Sujet : Bienvenue chez Gustatif\n"
+                            + "Corps :\n"
+                            + "Bonjour "+ cl.getPrenom() + ",\n");
+        if(etat == 1){
+            System.out.println("Nous vous confirmons votre inscription au service Gustat'IF. "
+                    + "Votre numero de client est "+ cl.getId() + ".\n");
+        }else{
+            System.out.println("Votre inscription au service gustat'IF a malheureusement échouée..."
+                    + " Merci de recommencer ultériorement.\n");
+        }
+    }
+    public static void envoiMailLivreur(LivreurHumain li, Commande co){
+        //si 0 echec si 1 reussi
+        System.out.println("Expediteur : gustatif@gustatif.com\n"
+                    + "Pour : " + li.getNom()+ " " + li.getPrenom() + " <" +li.getMail() +">\n"
+                            + "Sujet :Livraison n°" + co.getID() + "\n"
+                            + "Corps :\n"
+                            + "Bonjour "+ li.getPrenom() + ",\n"
+                            +"Merci d'effectuer cette livraison dès maintenant tout en respectant le code de la route >.^\n"
+                            + "Le Chef");
+
+    }
 }
